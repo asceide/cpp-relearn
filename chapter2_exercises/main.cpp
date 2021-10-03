dfd28b422e3a43792d93c37da601154049d34f01
Patrick Alarcon <personal@ichaiv.al>
2021-10-03 18:50:36 -0400

    Finished chapter 2

---
+++ chapter2_exercises/main.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main(){ //C++ programs start by executing the function main
+    /* Exercise 1: Change to two lines aying Hello, programming! Here we go!"
+    cout << "Hello, programming!\nHere we go!\n";
+    keep_window_open(); //Wait for a character to be entered, used for Windows, in order to exit.
+    return 0;
+     */
+
+    /* Exercise 2: Make instructions for a computer to move the from the dining room to the bathroom in the second floor */
+    cout << "Stand up\nTurn Around\nMove Forward to stairs\nStop\nTurn 90 degrees clockwise\nMove forward, up the stairs\n"
+            "Stop\nTurn 180 degrees counter-clockwise\nMove forward\nStop\nOpen door\nMove forward and enter\nStop\nTurn 90 degrees counter clockwise\nClose the door\n";
+            keep_window_open();
+            return 0;
+
+            /* The other 2 exercises are redundant for me so I am not going to do them" */
+}
---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+
+int main(){ //C++ programs start by executing the function main
+    cout << "Hello World\n"; // output "Hello World!"
+    keep_window_open(); //Wait for a character to be entered, used for Windows, in order to exit.
+    return 0;
+}
